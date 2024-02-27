6a02e70f23ac091bc49baaaa941c4362964a7ec4
cmorales1995 <124088173+cmorales1995@users.noreply.github.com>
2024-02-27 05:54:19 -0800

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"address.data","l":"AddressBook"},{"p":"address","l":"AddressBookApplication"},{"p":"address.data","l":"AddressBookTest"},{"p":"address.data","l":"addressEntry"},{"p":"address","l":"Menu"},{"p":"address.data","l":"MenuTest"}]
